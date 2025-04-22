b20c1ffb51490485666b5e2652495c9324e9384e
Juleslgc <venturajules99@gmail.com>
2025-04-22 11:22:41 +0200

    docs: add docs in the function

--- free_token.c
+++ free_token.c
@@ -1,6 +1,12 @@
 #include "shell.h"
 /**
+ * free_token - Frees a NULL-terminated array of strings.
+ * @token: The array of strings to be freed.
+ *
+ * This function iterates through a NULL-terminated array of dynamically
+ * allocated strings, frees each string, and then frees the array itself.
  * 
+ * Return: 0 on success, 1 if the input is NULL.
  */
 
 int free_token(char **token)
